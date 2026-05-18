a22853a5466744f552601ed82b8de2e84324d1fb
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-05-18 13:51:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+// import java.lang.reflect.Array;
+// import java.util.ArrayList;
+
+// class Main {
+    
+//     public static void main(String[]args){
+//         ArrayList<Integer> list=new ArrayList<>();
+//         list.add(5);
+//         list.add(6);
+//         list.add(7);
+//         ArrayList<Integer> newlist=new ArrayList<>();
+
+
+//     }
+// }
+
+
+
+class hello{
+    public static void main(String[] args) {
+        int mid=0;
+        int
+    }
+}
+
