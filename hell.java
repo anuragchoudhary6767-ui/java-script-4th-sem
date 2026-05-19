b8df643494a628b7bbb45a05132c6f9f6416a2ac
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-05-19 23:34:16 +0530

    Add files via upload

---
+++ hell.java
@@ -0,0 +1,58 @@
+// import java.util.ArrayList;
+
+// public class target {
+//     public static void main(String[] args) {
+//         ArrayList<Integer> list=new ArrayList<>();
+//         list.add(1);
+//         list.add(4);
+//         list.add(7);
+//         list.add(8);
+//         list.add(9);
+        
+//         // System.out.println("Before removal:");
+//         // for (Integer a : list) {
+//         //     System.out.println(a);
+//         // }
+
+//         // mid value=7;
+
+//         System.out.println(list);
+        
+//     }
+
+    
+// }
+
+
+
+
+
+
+// class hello{
+//     public static void main(String[] args) {
+//         String str="abcde";
+//         StringBuilder sb=new StringBuilder(str);
+//         sb.setCharAt(2,'z');
+//         System.out.println(sb);
+//     }
+// }
+
+
+
+
+
+
+// class hell{
+//     public static void main(String[] args) {
+//         System.out.println("hello");
+//         int a=5;
+//         int b=0;
+//         int c= a/b;
+//         System.out.println(c);
+//         System.out.println("byyy");
+//     }
+// }
+
+
+
+
